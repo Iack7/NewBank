29f6ed3106043ea2bdb7f5ec3be308f7e2570ff1
ThomasKopsch <62077151+ThomasKopsch@users.noreply.github.com>
2020-03-14 18:00:28 +0100

    Handling erroneous user data
    So far, the case that the user provides incorrect account information was not handled properly. Now the user is asked to correct the data, if they are wrong. Before, the server sent the message 'null' repeatedly.

--- src/newbank/server/NewBankClientHandler.java
+++ src/newbank/server/NewBankClientHandler.java
@@ -22,27 +22,30 @@ public class NewBankClientHandler extends Thread{
 	public void run() {
 		// keep getting requests from the client and processing them
 		try {
-			// ask for user name
-			out.println("Enter Username");
-			String userName = in.readLine();
-			// ask for password
-			out.println("Enter Password");
-			String password = in.readLine();
-			out.println("Checking Details...");
-			// authenticate user and get customer ID token from bank for use in subsequent requests
-			CustomerID customer = bank.checkLogInDetails(userName, password);
-			// if the user is authenticated then get requests from the user and process them 
-			if(customer != null) {
-				out.println("Log In Successful. What do you want to do?");
-				while(true) {
-					String request = in.readLine();
-					System.out.println("Request from " + customer.getKey());
-					String responce = bank.processRequest(customer, request);
-					out.println(responce);
+			while(true) {
+				// ask for user name
+				out.println("Enter Username");
+				String userName = in.readLine();
+				// ask for password
+				out.println("Enter Password");
+				String password = in.readLine();
+				out.println("Checking Details...");
+				// authenticate user and get customer ID token from bank for use in subsequent requests
+				CustomerID customer = bank.checkLogInDetails(userName, password);
+				// if the user is authenticated then get requests from the user and process them 
+				if(customer != null) {
+					out.println("Log In Successful. What do you want to do?");
+					while(true) {
+						String request = in.readLine();
+						System.out.println("Request from " + customer.getKey());
+						String responce = bank.processRequest(customer, request);
+						out.println(responce);
+					}
 				}
-			}
-			else {
-				out.println("Log In Failed");
+				else {
+					out.println("Log In Failed");
+					out.println("Please try again!\n");
+				}
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
